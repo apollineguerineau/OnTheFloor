09bd031c9a1563e0d0759a1ffa91361ef271ec9d
Apolline Guérineau <guerineau.apolline@gmail.com>
2025-12-27 13:03:17 +0100

    ✨ Add UserDAO with create and get methods

---
+++ src/data/dao/user_dao.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+from src.data.models import User
+
+class UserDAO:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_by_username(self, username: str) -> User | None:
+        return (
+            self.db.query(User)
+            .filter(User.username == username)
+            .one_or_none()
+        )
+
+    def create(self, username: str) -> User:
+        user = User(username=username)
+        self.db.add(user)
+        self.db.commit()
+        self.db.refresh(user)
+        return user
+
+
+    def get_by_id(self,user_id: int) -> User | None:
+        return self.db.get(User, user_id)
+
