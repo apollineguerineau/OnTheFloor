23835fafbac109f44e93a2d378bfdd6033033652
Apolline Guérineau <guerineau.apolline@gmail.com>
2025-12-27 13:03:24 +0100

    ✨ Add UserService with create_user and get_user

---
+++ src/services/user_service.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+from src.data.dao.user_dao import UserDAO
+from src.data.models import User
+from src.api.schemas.user import UserCreate
+
+class UserService:
+    def __init__(self, db: Session):
+        self.dao = UserDAO(db)
+
+    def create_user(self, dto: UserCreate):
+        existing = self.dao.get_by_username(dto.username)
+
+        if existing is not None:
+            raise ValueError("User already exists")
+
+        return self.dao.create(dto.username)
+   
+    def get_user(self,user_id: int) -> User | None:
+        return self.dao.get_by_id(user_id)
